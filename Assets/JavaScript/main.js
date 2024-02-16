24f5f29b234385af5df57a981b268753d5c3d320
Shivam0802 <shivamk08001@gmail.com>
2024-02-16 18:57:22 +0530

    First Website

---
+++ Assets/JavaScript/main.js
@@ -0,0 +1,8 @@
+function submitForm(){
+  //debugger;
+  var username = document.getElementById("txtName").value;
+  var password = document.getElementById("pwd").value;
+  let data={username:username,password:password};
+  console.log(data);
+  
+}
